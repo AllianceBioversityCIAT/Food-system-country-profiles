0c61255f0f38ab52ca18361cee29d9ca72031675
Miguel A. Buritica O <mburitica@cafetosoftware.com>
2022-09-26 16:13:01 -0500

    CPW-81: Tab type box design to show the indicators of each component of the upper part.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Necessary steps to run the project, being a seed is to copy the whole project and change it to your liking.
 
-### Version 0.15.0 ###
+### Version 0.16.0 ###
 
 
 ### Getting started ###
--- src/static/js/page-dashboard.js
+++ src/static/js/page-dashboard.js
@@ -5,9 +5,31 @@ jQuery( document ).ready( ( $ ) => {
     $( this ).addClass( "active" );
   } );
 
+  $( '.indicator-option' ).click( function () {
+    $( '.indicator-option' ).removeClass( 'active' );
+    $( this ).addClass( "active" );
+  } );
+
   $( '.note-closed' ).click( function () {
     $( '.note-indicators' ).hide();
   } );
+
+  //When page loads...
+  $(".tab_content").hide(); //Hide all content
+  $("ul.tabs li:first").addClass("active").show(); //Activate first tab
+  $(".tab_content:first").show(); //Show first tab content
+
+  //On Click Event
+  $("ul.tabs li").click(function() {
+
+    $("ul.tabs li").removeClass("active"); //Remove any "active" class
+    $(this).addClass("active"); //Add "active" class to selected tab
+    $(".tab_content").hide(); //Hide all tab content
+
+    var activeTab = $(this).find("a").attr("href"); //Find the href attribute value to identify the active tab + content
+    $(activeTab).fadeIn(); //Fade in the active ID content
+    return false;
+  });
 
   // Chart Bar Grouped.
   const xLabels           = {
