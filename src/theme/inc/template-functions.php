76bdfb240163d272475689002cd1db777cf1408b
Miguel A. Buritica O <mburitica@cafetosoftware.com>
2022-10-12 11:04:35 -0500

    CPW-165: Numeric fields for indicator data.

--- src/theme/inc/template-functions.php
+++ src/theme/inc/template-functions.php
@@ -85,52 +85,40 @@ function wp_update_fields_indicators( $post_id ) {
             $fields = get_fields( $post_id );
 
             // Fields Country.
-            $c_difference          = (float) $fields[ 'country_last_measure' ] - $fields[ 'country_initial_measure' ];
-            $c_growth_rate         = round( (float) ( $c_difference / $fields[ 'country_initial_measure' ] ) * 100, 2 );
-            $c_final_status_growth = final_status_growth( $c_growth_rate, $fields[ 'type_growth_indicator' ][ 'value' ] );
-            $c_consolidated_value  = consolidated_value( $c_final_status_growth );
+            $c_calculated_values = fscp_calculate_group_indicators( $fields[ 'country_initial_measure' ], $fields[ 'country_last_measure' ], $fields[ 'type_growth_indicator' ][ 'value' ] );
 
             // Update fields country.
-            update_field( 'country_difference', $c_difference, $post_id );
-            update_field( 'country_growth_rate', $c_growth_rate, $post_id );
-            update_field( 'country_final_status', $c_final_status_growth, $post_id );
-            update_field( 'country_consolidated_value', $c_consolidated_value, $post_id );
+            update_field( 'country_difference', $c_calculated_values[ 'difference' ], $post_id );
+            update_field( 'country_growth_rate', $c_calculated_values[ 'growth_rate' ], $post_id );
+            update_field( 'country_final_status', $c_calculated_values[ 'final_status' ], $post_id );
+            update_field( 'country_consolidated_value', $c_calculated_values[ 'consolidated' ], $post_id );
 
             // Fields Geographic neighbors.
-            $gn_difference          = (float) $fields[ 'gn_last_measure' ] - $fields[ 'gn_initial_measure' ];
-            $gn_growth_rate         = round( (float) ( $gn_difference / $fields[ 'gn_initial_measure' ] ) * 100, 2 );
-            $gn_final_status_growth = final_status_growth( $gn_growth_rate, $fields[ 'type_growth_indicator' ][ 'value' ] );
-            $gn_consolidated_value  = consolidated_value( $gn_final_status_growth );
+            $gn_calculated_values = fscp_calculate_group_indicators( $fields[ 'gn_initial_measure' ], $fields[ 'gn_last_measure' ], $fields[ 'type_growth_indicator' ][ 'value' ] );
 
             // Update fields Geographic neighbors.
-            update_field( 'gn_difference', $gn_difference, $post_id );
-            update_field( 'gn_growth_rate', $gn_growth_rate, $post_id );
-            update_field( 'gn_final_status', $gn_final_status_growth, $post_id );
-            update_field( 'gn_consolidated_value', $gn_consolidated_value, $post_id );
+            update_field( 'gn_difference', $gn_calculated_values[ 'difference' ], $post_id );
+            update_field( 'gn_growth_rate', $gn_calculated_values[ 'growth_rate' ], $post_id );
+            update_field( 'gn_final_status', $gn_calculated_values[ 'final_status' ], $post_id );
+            update_field( 'gn_consolidated_value', $gn_calculated_values[ 'consolidated' ], $post_id );
 
             // Fields Countries with similar GDP.
-            $gdp_difference          = (float) $fields[ 'gdp_last_measure' ] - $fields[ 'gdp_initial_measure' ];
-            $gdp_growth_rate         = round( (float) ( $gdp_difference / $fields[ 'gdp_initial_measure' ] ) * 100, 2 );
-            $gdp_final_status_growth = final_status_growth( $gdp_growth_rate, $fields[ 'type_growth_indicator' ][ 'value' ] );
-            $gdp_consolidated_value  = consolidated_value( $gdp_final_status_growth );
+            $gdp_calculated_values = fscp_calculate_group_indicators( $fields[ 'gdp_initial_measure' ], $fields[ 'gdp_last_measure' ], $fields[ 'type_growth_indicator' ][ 'value' ] );
 
             // Update fields Countries with similar GDP.
-            update_field( 'gdp_difference', $gdp_difference, $post_id );
-            update_field( 'gdp_growth_rate', $gdp_growth_rate, $post_id );
-            update_field( 'gdp_final_status', $gdp_final_status_growth, $post_id );
-            update_field( 'gdp_consolidated_value', $gdp_consolidated_value, $post_id );
+            update_field( 'gdp_difference', $gdp_calculated_values[ 'difference' ], $post_id );
+            update_field( 'gdp_growth_rate', $gdp_calculated_values[ 'growth_rate' ], $post_id );
+            update_field( 'gdp_final_status', $gdp_calculated_values[ 'final_status' ], $post_id );
+            update_field( 'gdp_consolidated_value', $gdp_calculated_values[ 'consolidated' ], $post_id );
 
             // Fields Global average.
-            $ga_difference          = (float) $fields[ 'ga_last_measure' ] - $fields[ 'ga_initial_measure' ];
-            $ga_growth_rate         = round( (float) ( $ga_difference / $fields[ 'ga_initial_measure' ] ) * 100, 2 );
-            $ga_final_status_growth = final_status_growth( $ga_growth_rate, $fields[ 'type_growth_indicator' ][ 'value' ] );
-            $ga_consolidated_value  = consolidated_value( $ga_final_status_growth );
+            $ga_calculated_values = fscp_calculate_group_indicators( $fields[ 'ga_initial_measure' ], $fields[ 'ga_last_measure' ], $fields[ 'type_growth_indicator' ][ 'value' ] );
 
             // Update fields Global average.
-            update_field( 'ga_difference', $ga_difference, $post_id );
-            update_field( 'ga_growth_rate', $ga_growth_rate, $post_id );
-            update_field( 'ga_final_status', $ga_final_status_growth, $post_id );
-            update_field( 'ga_consolidated_value', $ga_consolidated_value, $post_id );
+            update_field( 'ga_difference', $ga_calculated_values[ 'difference' ], $post_id );
+            update_field( 'ga_growth_rate', $ga_calculated_values[ 'growth_rate' ], $post_id );
+            update_field( 'ga_final_status', $ga_calculated_values[ 'final_status' ], $post_id );
+            update_field( 'ga_consolidated_value', $ga_calculated_values[ 'consolidated' ], $post_id );
         }
     }
 
@@ -213,14 +201,39 @@ function fscp_filter_cars_by_taxonomies( $post_type, $which ) {
 
 add_action( 'restrict_manage_posts', 'fscp_filter_cars_by_taxonomies', 10, 2 );
 
+function fscp_calculate_group_indicators( $initial_measure, $last_measure, $type_growth ) {
+    $calculate_result = [ 'difference' => 0, 'growth_rate' => 0, 'final_status' => 0, 'consolidated' => 0 ];
+
+    if ( $last_measure == 0 && $initial_measure == 0  ) {
+        $calculate_result[ 'difference' ]   = 0;
+        $calculate_result[ 'growth_rate' ]  = 0;
+        $calculate_result[ 'final_status' ] = final_status_growth( $calculate_result[ 'growth_rate' ], $type_growth );
+        $calculate_result[ 'consolidated' ] = consolidated_value( $calculate_result[ 'final_status' ] );
+
+    } else if ( $initial_measure == 0 ) {
+        $calculate_result[ 'difference' ]   = 0;
+        $calculate_result[ 'growth_rate' ]  = 0;
+        $calculate_result[ 'final_status' ] = final_status_growth( $calculate_result[ 'growth_rate' ], $type_growth );
+        $calculate_result[ 'consolidated' ] = consolidated_value( $calculate_result[ 'final_status' ] );
+
+    } else {
+        $calculate_result[ 'difference' ]   = (float) $last_measure - $initial_measure;
+        $calculate_result[ 'growth_rate' ]  = round( (float) ( $calculate_result[ 'difference' ] / $initial_measure ) * 100, 2 );
+        $calculate_result[ 'final_status' ] = final_status_growth( $calculate_result[ 'growth_rate' ], $type_growth );
+        $calculate_result[ 'consolidated' ] = consolidated_value( $calculate_result[ 'final_status' ] );
+    }
+
+    return $calculate_result;
+}
+
 function fscp_calculate_component_value( $component, $count ) {
     $component_result = [ 'c' => $count, 'gn' => $count, 'gdp' => $count, 'ga' => $count, 'total_component' => $count ];
 
     foreach ( $component as $value ) {
-        $component_result['c'] = $component_result['c'] + ( $value->custom['country_consolidated_value'] );
-        $component_result['gn'] = $component_result['gn'] + ( $value->custom['gn_consolidated_value'] );
-        $component_result['gdp'] = $component_result['gdp'] + ( $value->custom['gdp_consolidated_value'] );
-        $component_result['ga'] = $component_result['ga'] + ( $value->custom['ga_consolidated_value'] );
+        $component_result[ 'c' ]   = $component_result[ 'c' ] + ( $value->custom[ 'country_consolidated_value' ] );
+        $component_result[ 'gn' ]  = $component_result[ 'gn' ] + ( $value->custom[ 'gn_consolidated_value' ] );
+        $component_result[ 'gdp' ] = $component_result[ 'gdp' ] + ( $value->custom[ 'gdp_consolidated_value' ] );
+        $component_result[ 'ga' ]  = $component_result[ 'ga' ] + ( $value->custom[ 'ga_consolidated_value' ] );
     }
 
     return $component_result;
